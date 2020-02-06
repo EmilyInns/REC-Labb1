62db8f1f041bb6a56decf064f0c99ba704b95397
Rikard <Rickard.forslund@live.se>
2020-02-06 09:39:11 +0100

    index added

--- src/main/java/JavaHTTPServer.java
+++ src/main/java/JavaHTTPServer.java
@@ -23,6 +23,7 @@ public class JavaHTTPServer implements Runnable{
     static final String DEFAULT_FILE = "index.html";
     static final String FILE_NOT_FOUND = "404.html";
     static final String METHOD_NOT_SUPPORTED = "not_supported.html";
+
     // port to listen connection
     static final int PORT = 8080;
 
